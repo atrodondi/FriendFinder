143a1620240a9e83c960bc53db58f121d4b1905a
Cheebachong <atrodondi@gmail.com>
2020-04-20 17:04:03 -0700

    working on logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@
      {
          name: "Buddy",
          photo: "https://images.unsplash.com/photo-1508280756091-9bdd7ef1f463?ixlib=rb-1.2.1&w=1000&q=80",
-         scores: [1,5,2,4,4,3,5,5,4,2,]
+         scores: [5,5,1,1,1,1,1,1,1,1]
      },
      {
          name: "Rosco",
          photo: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313__340.jpg",
-         scores: [4,2,5,1,3,3,4,2,2,5]
+         scores: [4,4,4,4,4,4,4,4,4,4]
      },
      {
          name: "Nathan",
          photo: "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg",
-         scores: [3,2,1,1,2,5,4,5,3,4]
+         scores: [5,5,5,5,5,5,5,5,5,5]
      },
      {
          name: "Neal",
          photo: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/01/12201051/cute-puppy-body-image.jpg",
-         scores: [5,4,3,4,4,2,1,2,5,5,]
+         scores: [1,1,1,1,1,1,1,1,1,1]
      },
      {
          name: "Bubbles",
          photo: "https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg",
-         scores:[4,2,1,3,3,5,3,2,4,1]
+         scores:[2,2,2,2,2,2,2,2,2,2]
      },
      {
          name: "Chloe",
          photo: "https://i.guim.co.uk/img/media/20098ae982d6b3ba4d70ede3ef9b8f79ab1205ce/0_0_969_1005/master/969.jpg?width=700&quality=85&auto=format&fit=max&s=470657ebd2a0e704df88997d393aea15",
-         scores:[4,3,2,5,5,4,3,4,3,1]
+         scores:[3,3,3,3,3,3,3,3,3,3]
      }
  ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,25 +8,43 @@
 
     // listening for a post to api friends, then....
     app.post("/api/friends", function(req,res){
+         // logic to compare req object scores to all scores existing objects in friends array?
         
-        console.log(req.body.scores);
-        console.log(friendsArray[0].scores);
+        let newScores = req.body.scores;
+      
+        console.log(newScores);
+        // for every friend object in the friend array..
         for (var i=0; i<friendsArray.length;i++){
             console.log(friendsArray[i].scores);
+            let newScores = req.body.scores;
+        let totalDiff = 0;
+        let largestDiff = 0;
+        let matchName = "";
+        let diffArray=[];
+            
+            //for each item in the score array in that  particular object that is an iteration of the friend array, if...
             for (var j =0;j<friendsArray[i].scores.length;j++){
                 console.log(friendsArray[i].scores[j]);
+                console.log(newScores[j]);
+                // let difference = Math.abs(newScores[j]-parseInt(friendsArray[i].scores[j]));
+                //  totalDiff +=difference;
+                //  diffArray.push({diff: totalDiff,name:friendsArray[i].name});
+                 
+                //  console.log(diffArray);
                 
-          
+                
             }
+            // console.log(diffArray);
+            // let max= Math.max(...diffArray)
+            // console.log(max);
+            // console.log("Index of max diffference: "+ diffArray.indexOf(max));
+            // let match = diffArray.indexOf(max);
+            // matchName = friendsArray[match].name;
+            // console.log(matchName);
         }
-        
-        
-        
-        // logic to compare req object scores to all scores existing objects in friends array
-
-
         friendsArray.push(req.body);
-res.json(true);
+        res.json(true);
+        
     });
 
 
