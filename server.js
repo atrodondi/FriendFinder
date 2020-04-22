afce6a021863928f00412842b7377fd91684e7ba
Cheebachong <atrodondi@gmail.com>
2020-04-22 14:11:26 -0700

    heroku again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require("express");
 // tells node that we are creating an express server
 var app = express ();
 
-var PORT =- process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
